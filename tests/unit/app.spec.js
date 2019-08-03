b50e3e8f243081cbe9ed872e13d6b8c61fcbdba3
Takeshi Hirosue <takeshi.hirosue@bigtreetc.com>
2019-08-03 15:57:16 +0900

    feat: wip test 2

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App.vue', () => {
+  it('タイトルが表示されること', () => {
+    const wrapper = shallowMount(App)
+    console.log(wrapper.text())
+    expect(wrapper.text()).toContain('担当者一覧')
   })
 })
