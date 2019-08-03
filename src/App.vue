3dc21773424f31a9339b0b63c5a35b1bf7c5d8b7
Takeshi Hirosue <takeshi.hirosue@bigtreetc.com>
2019-08-03 16:44:31 +0900

    feat: add loading

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@
       <hr />
       <section id="result-list" class="hero">
         <table
-          v-if="0 < results.length"
+          v-if="0 < results.length && !loding"
           class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
         >
           <thead>
@@ -66,43 +66,54 @@
             </tr>
           </tbody>
         </table>
+        <div class="spinner" v-if="loding">
+          <div class="cube1"></div>
+          <div class="cube2"></div>
+        </div>
       </section>
-      {{results}}
     </div>
   </div>
 </template>
 
 <script>
-import _ from 'lodash';
+import _ from "lodash";
 
 export default {
   name: "app",
   data() {
     return {
       firstName: "",
-      results: []
+      results: [],
+      loding: false,
     };
   },
   methods: {
+    sleep: msec => new Promise(resolve => setTimeout(resolve, msec)),
     shuffle() {
       this.results = _.shuffle(this.results);
     },
     search() {
-      console.log(this.firstName);
+      this.loding = true;
       fetch(
         "http://localhost:18081/admin/api/staff?firstName=" + this.firstName
       )
-        .then(response => response.json())
+        .then(async response => {
+          await this.sleep(2000);
+          return response.json();
+        })
         .then(myJson => {
+          this.loding = false;
           this.results = myJson.data;
-          console.log(this.results);
+        })
+        .catch(() => {
+          this.loding = false;
         });
     }
   }
 };
 </script>
 
-<style>
+<style scoped href="https://cdn.rawgit.com/ConnorAtherton/loaders.css/master/loaders.min.css">
 .v-move {
   transition: transform 1s;
 }
@@ -112,5 +123,64 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+}
+.spinner {
+  margin: 100px auto;
+  width: 40px;
+  height: 40px;
+  position: relative;
+}
+.cube1,
+.cube2 {
+  background-color: #333;
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  -webkit-animation: sk-cubemove 1.8s infinite ease-in-out;
+  animation: sk-cubemove 1.8s infinite ease-in-out;
+}
+.cube2 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+@-webkit-keyframes sk-cubemove {
+  25% {
+    -webkit-transform: translateX(42px) rotate(-90deg) scale(0.5);
+  }
+  50% {
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-180deg);
+  }
+  75% {
+    -webkit-transform: translateX(0px) translateY(42px) rotate(-270deg)
+      scale(0.5);
+  }
+  100% {
+    -webkit-transform: rotate(-360deg);
+  }
+}
+@keyframes sk-cubemove {
+  25% {
+    transform: translateX(42px) rotate(-90deg) scale(0.5);
+    -webkit-transform: translateX(42px) rotate(-90deg) scale(0.5);
+  }
+  50% {
+    transform: translateX(42px) translateY(42px) rotate(-179deg);
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-179deg);
+  }
+  50.1% {
+    transform: translateX(42px) translateY(42px) rotate(-180deg);
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-180deg);
+  }
+  75% {
+    transform: translateX(0px) translateY(42px) rotate(-270deg) scale(0.5);
+    -webkit-transform: translateX(0px) translateY(42px) rotate(-270deg)
+      scale(0.5);
+  }
+  100% {
+    transform: rotate(-360deg);
+    -webkit-transform: rotate(-360deg);
+  }
 }
 </style>
